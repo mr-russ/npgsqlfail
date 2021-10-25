8e0d4290074d706eda048f7e13260a66213c9646
Russell Smith <russell.smith@cornerstonesolutions.com.au>
2021-10-26 10:47:06 +1100

    Pgsql transaction fail.

--- npgsqlfail/Program.cs
+++ npgsqlfail/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Common;
+using System.Transactions;
 
 namespace npgsqlfail
 {
@@ -6,7 +8,74 @@ namespace npgsqlfail
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Allow the application to loop 5 times, then exit.
+            // What really happens is that we get a nested transaction error
+            // on the second iteration as it's not cleaned up correctly.
+            int i = 0;
+
+            while (i++ < 5)
+            {
+                try
+                {
+                    using (var scope = new TransactionScope())
+                    {
+                        using (var connection = CreateOpenConnection()) { }
+
+                        try
+                        {
+                            try
+                            {
+                                using (var scope2 = new TransactionScope())
+                                {
+                                    throw new Exception("test Exception");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                // Required exception to trigger the nested transaction issue.
+                            }
+
+                            try
+                            {
+                                using (var connection = CreateOpenConnection()) { }
+                            }
+                            //catch (TransactionException ex)
+                            //{
+                            //   Will successfully work if you don't attempt the save and catch the transaction exception.
+                            //}
+                            catch (Exception ex)
+                            {
+                                using (var connection = CreateOpenConnection()) { }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            scope.Complete();
+                            continue;
+                        }
+                    }
+                }
+                catch (System.Transactions.TransactionException e)
+                {
+
+                }
+            }
+        }
+
+        public static System.Data.Common.DbConnection CreateOpenConnection()
+        {
+            var builder = new DbConnectionStringBuilder
+            {
+                ConnectionString = "// connection string here."
+            };
+
+            builder["Enlist"] = true;
+
+            var connection = Npgsql.NpgsqlFactory.Instance.CreateConnection();
+            connection.ConnectionString = builder.ConnectionString;
+
+            connection.Open();
+            return connection;
         }
     }
 }
